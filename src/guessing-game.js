12a6900323bf4404c9f073f6f3ea9f94f972469a
e.akhantev <e.akhantev@s7.ru>
2023-09-13 02:59:44 +0400

    feat: completed the class `GuessingGame`

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
-    }
+        this.first = min;
+        this.last = max;
+    };
 
     guess() {
-
-    }
+        this.mid = Math.round((this.last + this.first) / 2);
+        return this.mid;
+    };
 
     lower() {
-
-    }
-
+        this.last = this.mid;
+    };
+    
     greater() {
-
-    }
-}
+        this.first = this.mid;
+    };
+};
 
 module.exports = GuessingGame;
